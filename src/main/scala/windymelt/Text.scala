889e36f5ed2113ac217ea231bb321146dbc910c9
Windymelt <windymelt@3qe.us>
2022-06-18 23:25:57 +0900

    refactor

--- src/main/scala/windymelt/Text.scala
+++ src/main/scala/windymelt/Text.scala
@@ -6,7 +6,7 @@ object Text {
   def apply(
       t: TextEvent,
       ctx: Converter.Ctx
-  ): Tuple2[Converter.Ctx, Seq[String]] = {
+  ): (Converter.Ctx, Seq[String]) = {
     val trimmed = t.text.trim
     if (!trimmed.isEmpty() && !trimmed.isBlank()) {
       // println(s"pushing [${trimmed}]")
